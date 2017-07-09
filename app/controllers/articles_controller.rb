a4be34e3a28f060cb820a04417f62eea3bb52818
Samantha Gongora <samantha.gongora@gmail.com>
2017-07-09 16:16:19 -0600

    adds flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,15 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
 
     redirect_to articles_path
   end
@@ -31,6 +35,8 @@ class ArticlesController < ApplicationController
 
   def update
     @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
 
     redirect_to article_path(@article)
   end
